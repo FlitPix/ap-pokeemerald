8a7ac9f804bd63974eeb238943538b37221446ba
Flit <8645405+FlitPix@users.noreply.github.com>
2026-09-06 10:59:18 -0400

    fix rom patching

--- rom.py
+++ rom.py
@@ -16,9 +16,9 @@ if TYPE_CHECKING:
 class PokemonEmeraldFlitPatchExtension(APPatchExtension):
     game = GAME_NAME
 
-    @staticmethod
-    def apply_bsdiff4(caller: APProcedurePatch, rom: bytes, patch: str) -> bytes:
-        rom_data = bytearray(rom)
+    #@staticmethod
+    #def apply_bsdiff4(caller: APProcedurePatch, rom: bytes, patch: str) -> bytes:
+    #    rom_data = bytearray(rom)
 
 class PokemonEmeraldFlitProcedurePatch(APProcedurePatch, APTokenMixin):
     game = GAME_NAME
